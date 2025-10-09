a6bf66b4646b7f85a8a380f9636b10e6f9304a26
819SauCe <joaovitor819oqueres@gmail.com>
2025-10-09 15:22:13 -0300

    add login page design

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./styles/global.scss";
 import { Home } from './pages/home';
 import { Login } from './pages/login';
 import { Register } from './pages/register';
--- front-end/src/pages/login.tsx
+++ front-end/src/pages/login.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import Button from '../components/Button';
 import Input from '../components/Input';
+import styles from '../styles/pages/Login.module.scss';
 import buttonStyles from '../styles/components/Button.module.scss';
 import inputStyles from '../styles/components/Input.module.scss';
-
 
 export function Login() {
   const [email, setEmail] = useState("");
@@ -15,7 +15,7 @@ export function Login() {
       return;
     }
 
-    fetch("http://localhost:3000/api/login", {
+    fetch(`http://localhost:3000/api/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
@@ -26,11 +26,21 @@ export function Login() {
   }
 
   return (
-    <>
-      <h1>login</h1>
-      <Input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" type="email" className={inputStyles.default_input} />
-      <Input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" className={inputStyles.default_input} />
-      <Button label="Click me" onClick={handleLogin} className={buttonStyles.default_button} />
-    </>
+    <main className={styles.main}>
+      <div className={styles.login_container}>
+        <div>
+          <h1 className={styles.title_login_container}>login</h1>
+        </div>
+        <div className={styles.input_container}>
+          <label htmlFor="email">Email:</label>
+          <Input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="example@example.com" type="email" className={inputStyles.default_input}/>
+          <label htmlFor="password">Password:</label>
+          <Input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="********" type="password" className={inputStyles.default_input}/>
+        </div>
+        <Button label="Click me" onClick={handleLogin} className={buttonStyles.default_button}/>
+        <p className={styles.forgot_password}>Forgot Password? <a href="/forgot-password">Click here</a></p>
+        <p className={styles.register_link}>Don't have an account? <a href="/register">Register</a></p>
+      </div>
+    </main>
   );
 }
